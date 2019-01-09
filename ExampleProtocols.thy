08582e15855560568f2167dc12a301ad658b1388
yudetamago <yudetamago.orz@gmail.com>
2019-01-09 15:47:15 +0900

    Add V params

--- ExampleProtocols.thy
+++ ExampleProtocols.thy
@@ -50,9 +50,9 @@ definition latest_estimates :: "state \<Rightarrow> validator \<Rightarrow> cons
 
 (* Lemma 5: Non-equivocating validators have at most one latest message *)
 lemma non_equivocating_validators_have_at_most_one_latest_message:
-  (* "\<forall> v \<in> v_set. v \<notin> equivocating_validators s \<Longrightarrow> card (latest_message s v) \<le> 1" *)
+  (* "\<forall> params v. v \<in> V params \<and> v \<notin> equivocating_validators s \<Longrightarrow> card (latest_message s v) \<le> 1" *)
   (* prove the contrapositive *)
-  "\<forall> v \<in> v_set. card (latest_message s v) > 1 \<Longrightarrow> v \<in> equivocating_validators s"
+  "\<forall> params v. v \<in> V params \<and> card (latest_message s v) > 1 \<Longrightarrow> v \<in> equivocating_validators s"
   apply(simp add: equivocating_validators_def latest_message_def)
   (* TODO *)
   sorry
