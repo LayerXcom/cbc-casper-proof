ab509d2b152a57e461587498714ab185cbaf977b
yudetamago <yudetamago.orz@gmail.com>
2019-01-15 13:05:45 +0900

    Add remainings in section 4.1

--- ExampleProtocols.thy
+++ ExampleProtocols.thy
@@ -52,4 +52,45 @@ definition latest_estimates :: "state \<Rightarrow> validator \<Rightarrow> cons
 lemma non_equivocating_validators_have_at_most_one_latest_message:
   "\<forall> params v. v \<in> V params \<and> v \<notin> equivocating_validators \<sigma> \<Longrightarrow> card (latest_message \<sigma> v) \<le> 1"
   using V.simps by blast
+
+(* Definition 4.10 *)
+fun message_order
+  where
+    "message_order (m1, m2) = (card (justification m1) \<ge> card (justification m2))"
+
+(* Lemma 6 *)
+(* TODO *)
+
+(* Lemma 7 *)       
+lemma monotonicity_of_justifications:
+  "\<forall> params \<sigma>' \<sigma>. is_valid_params params \<and> \<sigma>' \<in> \<Sigma> params \<and> \<sigma> \<in> \<Sigma> params \<Longrightarrow>
+  m' \<in> later (m, \<sigma>) \<Longrightarrow> justification m  \<subseteq> justification m'"
+  by (meson V.simps is_valid_params_def is_valid_validators_def)
+
+(* Lemma 8 *)       
+lemma the_minimal_elements_are_the_latest_messages:
+  "\<forall> params \<sigma>' \<sigma>. is_valid_params params \<and> \<sigma>' \<in> \<Sigma> params \<and> \<sigma> \<in> \<Sigma> params \<Longrightarrow>
+  \<forall>m'. m' \<in> from_sender (v, \<sigma>) \<and> message_order (m, m')\<Longrightarrow>                   
+  m \<in> latest_message \<sigma> v"
+  apply (rule, rule, rule)
+proof -
+  fix params
+  assume hyp: "is_valid_params params"
+  assume "\<forall>m'. m' \<in> from_sender (v, \<sigma>) \<and> message_order (m, m') \<Longrightarrow>  m \<notin> latest_message \<sigma> v"
+
+  have
+    "  \<forall>m'. m' \<in> from_sender (v, \<sigma>) \<and> message_order (m, m') \<and> \<not> (m \<in> latest_message \<sigma> v) \<Longrightarrow>
+    \<forall>m'. \<exists> ms. m' \<in> from_sender (v, \<sigma>) \<and> message_order (m, m') \<and> ms \<in> later_from (m, v, \<sigma>)"
+    apply (simp add: latest_message_def)                                 
+    by auto
+
+  oops
+
+(* Lemma 10 *)
+lemma observed_non_equivocating_validators_have_one_latest_message:
+  "\<forall> params \<sigma> v. is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> v \<in> V params \<Longrightarrow>
+  v \<in> observed \<sigma> \<and> v \<notin> equivocating_validators \<sigma> \<Longrightarrow> 
+  card(latest_message \<sigma> v) == 1"
+  apply (simp add: equivocating_validators_def latest_message_def)
+  sorry
 end
