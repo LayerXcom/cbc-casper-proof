f87df71d9c51e8fc2e242d6e72d89551883338ed
中村龍矢 <nrryuya@gmail.com>
2019-01-19 17:58:27 +0900

    Add message_in_state_is_valid

--- SafetyOracles.thy
+++ SafetyOracles.thy
@@ -103,11 +103,46 @@ fun minimal_transitions :: "params \<Rightarrow> (state * state) set"
     "minimal_transitions params = {(\<sigma>, \<sigma>') | \<sigma> \<sigma>'. \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params \<and> is_future_state (\<sigma>', \<sigma>) \<and> \<sigma> \<noteq> \<sigma>'
       \<and> (\<nexists> \<sigma>''. \<sigma>'' \<in> \<Sigma> params \<and> is_future_state (\<sigma>'', \<sigma>) \<and> is_future_state (\<sigma>', \<sigma>'') \<and> \<sigma> \<noteq> \<sigma>'' \<and> \<sigma>'' \<noteq> \<sigma>')}"
 
+lemma \<Sigma>t_is_subset_of_\<Sigma> :
+  "\<forall> params \<sigma> m. is_valid_params params
+  \<longrightarrow> \<Sigma>t params \<subseteq> \<Sigma> params"
+  using \<Sigma>t.simps by blast
+
+lemma message_in_state_is_valid :
+  "\<forall> params \<sigma> m. is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> \<sigma>
+  \<longrightarrow>  m \<in> M params"
+  apply (rule, rule, rule, rule)
+proof -
+  fix params \<sigma> m
+  assume "is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> \<sigma>"
+  have 
+    "\<exists> n \<in> \<nat>. \<sigma> \<in> \<Sigma>_i params n"
+    using \<Sigma>_def \<open>is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> \<sigma>\<close> by auto
+  moreover have
+    "\<exists> n \<in> \<nat>. \<sigma> \<in> \<Sigma>_i params n
+    \<longrightarrow> \<sigma> \<in> Pow (M_i params (n - 1))"
+    by (metis One_nat_def \<Sigma>_i.simps(1) \<open>is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> \<sigma>\<close> diff_Suc_1 diff_add_inverse empty_iff insert_iff of_nat_1 of_nat_Suc of_nat_in_Nats)
+  moreover have
+    "\<exists> n \<in> \<nat>. \<sigma> \<in> Pow (M_i params (n - 1))  
+    \<longrightarrow> (m \<in> \<sigma> \<longrightarrow> m \<in> M_i params (n - 1))"
+    using calculation(2) by blast
+  moreover have
+    "\<exists> n \<in> \<nat>. m \<in> M_i params (n - 1)
+    \<Longrightarrow> \<exists> n'\<in> \<nat>. m \<in> M_i params n'"
+    sorry
+  moreover have
+    "\<exists> n \<in> \<nat>. m \<in> M_i params n
+    \<Longrightarrow> m \<in> M params"
+    using M_def by blast 
+  ultimately show
+    "m \<in> M params"
+    by (smt PowD \<Sigma>_i.elims \<open>is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> \<sigma>\<close> empty_iff insert_iff mem_Collect_eq subsetCE)
+qed
+
 lemma state_is_subset_of_M :
-  "\<forall> params. is_valid_params params
-  \<longrightarrow> (\<forall> \<sigma>. \<sigma> \<in> \<Sigma> params \<and> \<sigma> \<subseteq> M params)"
-  apply (simp add: \<Sigma>_def M_def)
-  sorry
+  "\<forall> params \<sigma>. is_valid_params params \<and> \<sigma> \<in> \<Sigma> params
+  \<longrightarrow>  \<sigma> \<subseteq> M params"
+  using message_in_state_is_valid by blast
 
 lemma state_difference_is_valid_message :
   "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> \<sigma>' \<in> \<Sigma> params
@@ -115,44 +150,62 @@ lemma state_difference_is_valid_message :
   \<longrightarrow> \<sigma>' - \<sigma> \<subseteq> M params"
   using state_is_subset_of_M by blast
 
-(* Lemma for minimal_transition_implies_receiving_a_single_message *)
-lemma no_intermidiate_implies_difference_is_singleton :
-  "\<forall> A B. A \<subseteq> B \<and> A \<noteq> B \<longrightarrow> (\<nexists> C. A \<subseteq> C \<and> C \<subseteq> B \<and> A \<noteq> C \<and> B \<noteq> C) \<longrightarrow> is_singleton (B - A)"
+lemma state_transition_by_message_receiving :
+  "\<forall> params \<sigma> m. is_valid_params params \<and> \<sigma> \<in> \<Sigma> params \<and> m \<in> M params
+  \<longrightarrow>  \<sigma> \<union> {m} \<in> \<Sigma> params"
   apply simp
-  by (smt DiffD1 DiffD2 Diff_eq_empty_iff Diff_insert_absorb Diff_subset double_diff insert_subset is_singleton_def psubsetI psubset_imp_ex_mem)
+  sorry
 
 (* A minimal transition corresponds to receiving a single new message with justification drawn from the initial
 protocol state *)
-lemma minimal_transition_implies_receiving_a_single_message :
+lemma minimal_transition_implies_recieving_a_single_message :
   "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-  \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>'- \<sigma>)"
-proof (rule ccontr)
-  assume "\<not> (\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-  \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>'- \<sigma>))"
+  \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (\<sigma>'- \<sigma>)"
+proof -
   have
     "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-    \<longrightarrow> \<not> is_singleton (M params \<inter> \<sigma>'- \<sigma>) \<longrightarrow> (M params \<inter> \<sigma>'- \<sigma>) = \<emptyset>
-          \<or> (\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2)"
-    by (meson is_singletonI')
-  moreover have
-    "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-    \<longrightarrow> (M params \<inter> \<sigma>'- \<sigma>) = \<emptyset> \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> False"
-    by (simp add: Int_absorb2 inf_commute state_is_subset_of_M)
-  moreover have
-    "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-    \<longrightarrow> (\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2) \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> False"
-    by (smt Int_absorb2 \<open>\<not> (\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
-  \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>'- \<sigma>))\<close> inf.idem inf_commute is_future_state.simps mem_Collect_eq minimal_transitions.simps no_intermidiate_implies_difference_is_singleton prod.inject state_is_subset_of_M)  
-  ultimately show False
-    by (meson \<open>\<not> (\<forall>params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>' - \<sigma>))\<close>)
+    \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>'- \<sigma>)"
+  proof (rule ccontr)
+    assume "\<not> (\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
+    \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>'- \<sigma>))"
+    have
+      "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
+      \<longrightarrow> \<not> is_singleton (M params \<inter> \<sigma>'- \<sigma>) \<longrightarrow> (M params \<inter> \<sigma>'- \<sigma>) = \<emptyset>
+            \<or> (\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2)"
+      by (meson is_singletonI')
+    moreover have
+      "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
+      \<longrightarrow> (M params \<inter> \<sigma>'- \<sigma>) = \<emptyset> \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> False"
+      by (simp add: Int_absorb2  inf_commute state_is_subset_of_M)
+    moreover have
+      "\<forall> params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params
+      \<longrightarrow> (\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2) \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> False"
+      apply (rule, rule, rule, rule)
+    proof -
+      fix params \<sigma> \<sigma>'
+      assume "is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params"
+      have
+        "(\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2 
+          \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params \<and> is_future_state (\<sigma>', \<sigma>) \<and> \<sigma> \<noteq> \<sigma>')
+         \<longrightarrow> (\<exists> m1 m2 \<sigma>''. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2
+            \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params \<and> is_future_state (\<sigma>', \<sigma>) \<and> \<sigma> \<noteq> \<sigma>'
+            \<and> \<sigma>'' = \<sigma> \<union> {m1} \<and> \<sigma>'' \<in> \<Sigma> params \<and> is_future_state (\<sigma>'', \<sigma>) \<and> is_future_state (\<sigma>', \<sigma>'') \<and> \<sigma> \<noteq> \<sigma>'' \<and> \<sigma>'' \<noteq> \<sigma>')"
+        apply simp
+        by (metis (no_types, hide_lams) Un_insert_right \<open>is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params\<close> insert_iff state_transition_by_message_receiving subset_insertI sup_bot.right_neutral) 
+      then show
+        "(\<exists> m1 m2. m1 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m2 \<in> (M params \<inter> \<sigma>'- \<sigma>) \<and> m1 \<noteq> m2) \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> False"
+        by (smt Pair_inject mem_Collect_eq minimal_transitions.simps)
+      qed
+    ultimately show False
+      by (meson \<open>\<not> (\<forall>params \<sigma> \<sigma>'. is_valid_params params \<and> \<sigma> \<in> \<Sigma>t params \<and> \<sigma>' \<in> \<Sigma>t params \<longrightarrow> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<longrightarrow> is_singleton (M params \<inter> \<sigma>' - \<sigma>))\<close>)
+  qed
+  then show ?thesis
+    by (metis (no_types, lifting) Diff_Diff_Int \<Sigma>t.simps double_diff mem_Collect_eq order_refl state_is_subset_of_M)
 qed
-
-
 
 (* Lemma 11: Minimal transitions do not change Later_From for any non-sender *)
 lemma later_from_not_affected_by_minimal_transitions :
-  "\<forall> params \<sigma> \<sigma>' m. is_valid_params params \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params
-  \<longrightarrow> m' = the_elem (\<sigma>' - \<sigma>)
+  "\<forall> params \<sigma> \<sigma>' m m'. is_valid_params params \<and> (\<sigma>, \<sigma>') \<in> minimal_transitions params \<and> m' = the_elem (\<sigma>' - \<sigma>)
   \<longrightarrow> (\<forall> v \<in> V params - {sender m'}. later_from (m, v, \<sigma>) = later_from (m, v, \<sigma>'))"
   oops
 
