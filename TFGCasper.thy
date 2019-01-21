f78c919d319682b505df6c3a412509e8374d1f5a
yudetamago <yudetamago.orz@gmail.com>
2019-01-21 16:43:46 +0900

    Fix block definition

--- TFGCasper.thy
+++ TFGCasper.thy
@@ -7,39 +7,46 @@ begin
 (* Section 4.4: Casper the Friendly GHOST *)
 
 (* Definition 4.23: Blocks *)
-datatype block_value = Block_value int
-datatype block_data = Block_data
-type_synonym block = "block_value + block_value * block_data"
-datatype block_params = 
-  Block_params "validator set * weight * threshold * block set * estimator_param"
+typedecl block
 
-fun blk :: "block_params \<Rightarrow> block set"
+type_synonym genesis = block
+
+type_synonym next_block_generator = "block \<Rightarrow> block set"
+
+datatype ghost_params = GHOST_params "genesis * next_block_generator"
+
+fun g :: "ghost_params \<Rightarrow> block"
   where
-    "blk(Block_params (_, _, _, b, _)) = b"
+    "g (GHOST_params (gblk, _)) = gblk"
 
-(* TODO *)
-fun B_i :: "block_params \<Rightarrow> nat \<Rightarrow> block set"
+fun blk_gen :: "ghost_params \<Rightarrow> next_block_generator"
   where
-    "B_i block_params i =  blk block_params"
+    "blk_gen (GHOST_params (_, bgen)) = bgen"
 
-fun B :: "block_params \<Rightarrow> block set"
+fun B_i :: "ghost_params \<Rightarrow> nat \<Rightarrow> block set"
+  where
+    "B_i gparams 0 = {g gparams}"
+  | "B_i gparams n = \<Union> {blk_gen gparams b | b. b \<in> B_i gparams (n - 1)}"
+
+fun B :: "ghost_params \<Rightarrow> block set"
   where
     "B block_params = \<Union> (B_i block_params `\<nat>)"
 
 (* Definition 4.24: Previous block resolver *)
-fun prev :: "block \<Rightarrow> block"
+fun prev :: "ghost_params \<Rightarrow> block \<Rightarrow> block set"
   where
-    "prev b = b" (* TODO *)
+    "prev gparams b = (if b = g gparams then {g gparams} else {b'. b' \<in> B gparams \<and> b \<in> blk_gen gparams b'})"
 
 (* Definition 4.25: n'th generation ansectr block *)
-fun n_cestor :: "(block * nat) \<Rightarrow> block"
+fun n_cestor :: "ghost_params \<Rightarrow> block \<Rightarrow> nat \<Rightarrow> block set"
   where
-    "n_cestor (b, n) = b" (* TODO *)
+    "n_cestor gparams b 0 = {b}"
+  | "n_cestor gparams b n = {b. \<forall> prevblk.  prevblk \<in> (prev gparams b) \<and>  (b \<in> n_cestor gparams prevblk (n-1))}"
 
 (* Definition 4.26: Blockchain membership *)
-fun blockchain_membership :: "(block * block) \<Rightarrow> bool" 
+fun blockchain_membership :: "ghost_params \<Rightarrow> (block * block) \<Rightarrow> bool"
   where
-    "blockchain_membership (b1, b2) = (\<exists> n. n \<in> \<nat> \<and> b1 = n_cestor(b2, n))"
+    "blockchain_membership gparams (b1, b2) = (\<exists> n. n \<in> \<nat> \<and> (b1 \<in> n_cestor gparams b2 n))"
 
 (* Definition 4.27: Score of a block *)
 fun score :: "block * state \<Rightarrow> real"
