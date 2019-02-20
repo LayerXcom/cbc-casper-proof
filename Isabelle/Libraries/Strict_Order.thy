18e5ea338243b8c71140ff5d4a026f96b9da4e0c
nrryuya <nrryuya@gmail.com>
2019-02-20 10:49:51 +0900

    Remove unused proof of strict_partial_order_on_finite_non_empty_set_has_maximal

--- Isabelle/Libraries/Strict_Order.thy
+++ Isabelle/Libraries/Strict_Order.thy
@@ -71,63 +71,6 @@ proof -
     by (metis card.insert_remove finite.cases)
 qed
 
-lemma strict_partial_order_on_finite_non_empty_set_has_maximal :
-  "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> (\<exists> x. maximal_on A r x)"
-  apply (rule, rule, rule)
-proof - 
-  assume "strict_partial_order r"
-  then have "(\<forall>a. (a, a) \<notin> r)" 
-    by (simp add: strict_partial_order_def irrefl_def) 
-  assume "finite A" 
-  assume "A \<noteq> \<emptyset>"
-  show "\<exists> x. maximal_on A r x"
-    apply (induction "card A - 1")
-  proof -
-    assume "0 = card A - 1"
-    then have "card A = 1"
-      by (metis One_nat_def Suc_pred \<open>A \<noteq> \<emptyset>\<close> \<open>finite A\<close> card_gt_0_iff)
-    then show "Ex (maximal_on A r)"
-      unfolding maximal_on_def
-      using \<open>(\<forall>a. (a, a) \<notin> r)\<close>
-      by (metis card_1_singletonE insertI1 singletonD)  
-  next 
-    fix n
-    assume "n = card A - 1 \<Longrightarrow> Ex (maximal_on A r)"
-    then have "Suc n = card A \<Longrightarrow> (\<exists> x. maximal_on A r x)"
-      by simp
-    assume "Suc n = card A - 1"
-    then have "\<exists> A' x. A = A' \<union> {x} \<and> card A' = Suc n \<and> x \<notin> A'"
-      using \<open>A \<noteq> \<emptyset>\<close> \<open>finite A\<close>
-      by (metis Un_commute add_diff_cancel_left' card_gt_0_iff card_insert_disjoint card_le_Suc_iff insert_is_Un not_le not_less_eq_eq plus_1_eq_Suc)
-    then obtain A' x where "A = A' \<union> {x} \<and> card A' = Suc n \<and> x \<notin> A'"
-      by auto
-    have "\<exists> x'. maximal_on A' r x'"
-      using \<open>Suc n = card A \<Longrightarrow> (\<exists> x. maximal_on A r x)\<close> 
-      sorry
-    (* Then we do case analysis. *)
-    then show "Ex (maximal_on A r)"
-      sorry
-  qed
-qed
-
-lemma strict_partial_order_on_finite_non_empty_set_has_maximal_by_contradiction :
-  "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> (\<exists> x. maximal_on A r x)"
-proof (rule ccontr)
-  (* NOTE: How to use \<not> for pure logic? #81 *)
-  assume "\<not> (strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> (\<exists> x. maximal_on A r x))"
-  then have "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> \<not> (\<exists> x. maximal_on A r x)"
-    by simp
-  then have "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> \<not> (\<exists> x. x \<in> A \<and> (\<forall> y. (x, y) \<in> r \<longrightarrow> y \<notin> A))"
-    by (simp add: maximal_on_def) 
-  then have "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> (\<forall> x. x \<notin> A \<or> (\<exists> y. (x, y) \<in> r \<and> y \<in> A))"
-    by simp 
-  then have "strict_partial_order r \<longrightarrow> finite A \<longrightarrow> A \<noteq> \<emptyset> \<longrightarrow> (\<forall> x \<in> A. (\<exists> y. (x, y) \<in> r \<and> y \<in> A))"
-    by simp 
-  then show False
-    using strict_partial_order_def irrefl_def trans_def
-    sorry
-qed
-
 lemma strict_partial_order_has_at_most_one_maximum :
   "strict_partial_order r
   \<longrightarrow> {x. maximum_on A r x} \<noteq> \<emptyset> 
