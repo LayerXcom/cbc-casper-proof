eee617cb5748efe759884ae291776c1b3da4cf82
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-24 19:04:51 +0900

    Validator to be a map with ByteString as key and value

--- Isabelle/CodeGeneration.thy
+++ Isabelle/CodeGeneration.thy
@@ -14,7 +14,8 @@ import qualified Data.ByteString as BS;
 import qualified Data.Map as M;
 
 newtype ConsensusValue = ConsensusValue (M.Map BS.ByteString BS.ByteString);
-newtype Validator = Validator BS.ByteString\<close>
+newtype Validator = Validator (M.Map BS.ByteString BS.ByteString);
+\<close>
 
 instantiation consensus_value :: equal
 begin 
--- Isabelle/GeneratedCode/src/Params.hs
+++ Isabelle/GeneratedCode/src/Params.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString as BS;
 import qualified Data.Map as M;
 
 newtype ConsensusValue = ConsensusValue (M.Map BS.ByteString BS.ByteString);
-newtype Validator = Validator BS.ByteString
+newtype Validator = Validator (M.Map BS.ByteString BS.ByteString);
+
 
 }
